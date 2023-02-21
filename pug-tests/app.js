f6be9b2b6bd7629ce6697627446dc0e7f4a6bbfd
northwatch <polka1416@gmail.com>
2023-02-21 13:39:55 +0300

    pug

---
+++ pug-tests/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use('/contact', (req, res) => {
+	res.render('contact', {
+		title: 'Мои контакты',
+		emailsVisible: true,
+		emails: ['polka@gmail.com', 'polka@yandex.ru'],
+		phone: '+1234567890',
+	});
+});
+
+app.use('/', (req, res) => {
+	res.send('Главная страница');
+});
+app.listen(3000);
