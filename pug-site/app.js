baa4038673010259c775967bc1b858b8e3e00acc
northwatch <polka1416@gmail.com>
2023-03-05 01:01:43 +0300

    product, product-card

--- pug-site/app.js
+++ pug-site/app.js
@@ -1,13 +1,29 @@
-const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
+let productsList = {};
 app.use('/products', (req, res) => {
-	res.render('products');
+	fetch('https://dummyjson.com/products')
+		.then((res) => res.json())
+		.then((json) => {
+			productsList = json;
+			console.log(productsList.products);
+		});
+	res.render('products', productsList);
 });
+
+// {
+//   images: productsList.images,
+//   title: productsList.title,
+//   price: productsList.price,
+//   rating: productsList.rating,
+//   rating: productsList.rating,
+// }
 
 app.get('/', function (req, res) {
 	res.render('index', { title: 'Hey', message: 'Hello there!' });
